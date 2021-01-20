0ead397b86dc92b91aaec06c3792e0c9849afee6
CrandJah <bujhtitxrf@gmail.com>
2021-01-20 23:31:38 +1100

    task (1. Spring mock [#422738])

--- src/test/java/ru/job4j/forum/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/RegControllerTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.forum.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -35,5 +37,16 @@ class RegControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+  }
+
+  @Test
+  @WithMockUser
+  public void whenRegUserThenRedirectOk() throws Exception {
+    this.mockMvc.perform(post("/reg").param("username", "username")
+                                     .param("password", "password"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+
   }
 }
